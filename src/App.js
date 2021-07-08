c6e3e8ca32f1d2aea7be3a86679a490680595045
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-07-09 02:40:15 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-function App() {
+const App = () => {
+  const profiles = [
+    { name: "1", age: 1 },
+    { name: "2", age: 2 },
+  ];
   return (
     <div>
-      <h1>hello</h1>
+      {profiles.map((profile) => {
+        return <User name={profile.name} age={profile.age} />;
+      })}
     </div>
   );
-}
+};
+const User = (props) => {
+  return (
+    <div>
+      name = {props.name}, age = {props.age}{" "}
+    </div>
+  );
+};
 
 export default App;
