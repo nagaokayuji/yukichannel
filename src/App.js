7f8cf56613d6ddb8ec2f044db8557a0770c65761
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-07-11 14:34:53 +0900

    add: federated sign in

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import Amplify from "aws-amplify";
+import React, { useEffect, useState } from "react";
+import Amplify, { Auth, Hub } from "aws-amplify";
 
 Amplify.configure({
-  aws_project_region: process.env.REACT_APP_AWS_PROJECT_REGION,
-  aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
-  aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
-  aws_user_pools_web_client_id: process.env.REACT_APP_AWS_USER_POOLS_CLIENT_ID,
+  Auth: {
+    region: "ap-northeast-1", //process.env.REACT_APP_AWS_COGNITO_REGION,
+    userPoolId: "ap-northeast-1_T9t0ZjajV", //process.env.REACT_APP_AWS_USER_POOLS_ID,
+    userPoolWebClientId: "6i67no96drvb941gajdt3lfv7f", //process.env.REACT_APP_AWS_USER_POOLS_CLIENT_ID,
+  },
 });
+function App() {
+  const [user, setUser] = useState(null);
 
-const App = () => {
-  const profiles = [
-    { name: "1", age: 1 },
-    { name: "2", age: 2 },
-  ];
+  useEffect(() => {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+        case "cognitoHostedUI":
+          getUser().then((userData) => setUser(userData));
+          break;
+        case "signOut":
+          setUser(null);
+          break;
+        case "signIn_failure":
+        case "cognitoHostedUI_failure":
+          console.log("Sign in failure", data);
+          break;
+        default:
+          console.warn("unexpected state");
+      }
+    });
+
+    getUser().then((userData) => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then((userData) => userData)
+      .catch(() => console.log("Not signed in"));
+  }
+
   return (
     <div>
-      {profiles.map((profile) => {
-        return <User name={profile.name} age={profile.age} />;
-      })}
+      <p>User: {user ? JSON.stringify(user.attributes) : "None"}</p>
+      {user ? (
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => Auth.federatedSignIn()}>
+          Federated Sign In
+        </button>
+      )}
     </div>
   );
-};
-const User = (props) => {
-  return (
-    <div>
-      name = {props.name}, age = {props.age}{" "}
-    </div>
-  );
-};
+}
 
 export default App;
