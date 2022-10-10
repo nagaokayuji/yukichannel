d5b2be4b15cd3067064bf8e5daa73e9d5d8ba5a2
minngk <taiyakihajinsei3@gmail.com>
2022-10-10 09:30:47 +0900

    create yuki introduction

---
+++ src/Yuki.tsx
@@ -0,0 +1,10 @@
+function Yuki() {
+    return (
+        <>
+            <div className="elementTitle">Yuki</div>
+            <div className="yukiImg"></div>
+        </>
+    )
+}
+
+export default Yuki
